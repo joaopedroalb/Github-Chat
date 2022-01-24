3a763fadefab668b1068814209918fb648e644ff
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-24 13:49:03 -0300

    Change home page with skynex components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
-import {Box,Button,Text,TextField,Image} from '@skynexui/components'
+import { Box, Button, Text, TextField, Image } from '@skynexui/components'
 import appConfig from '../../config.json'
 
 function GlobalStyle() {
     return (
-      <style global jsx>{`
+        <style global jsx>{`
         * {
           margin: 0;
           padding: 0;
           box-sizing: border-box;
           list-style: none;
-          background:black;
         }
         body {
           font-family: 'Open Sans', sans-serif;
@@ -29,14 +28,14 @@ function GlobalStyle() {
         /* ./App fit Height */ 
       `}</style>
     );
-  }
+}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
     return (
         <>
-        <Tag>{props.children}</Tag>
-        <style jsx>{`
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
                 ${Tag} {
                     color: ${appConfig.theme.colors.neutrals['000']};
                     font-size: 24px;
@@ -47,17 +46,112 @@ function Title(props) {
     );
 }
 
-function HomePage() {
+export default function HomePage() {
+    const username = 'joaopedroalb';
+
     return (
-        <div>
-            <GlobalStyle/>
+        <>
+            <GlobalStyle />
+            <Box
+                styleSheet={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    backgroundColor: appConfig.theme.colors.primary[500],
+                    backgroundImage: 'url(https://static.vecteezy.com/system/resources/previews/003/337/350/non_2x/pink-shrimps-seafood-japanese-chinese-cuisine-pattern-banner-vector.jpg)',
+                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: {
+                            xs: 'column',
+                            sm: 'row',
+                        },
+                        width: '100%', maxWidth: '700px',
+                        borderRadius: '5px', padding: '32px', margin: '16px',
+                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }}
+                >
+                    {/* Formulário */}
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                            width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                        }}
+                    >
+                        <Title tag="h2">Boas vindas de volta!</Title>
+                        <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                            {appConfig.name}
+                        </Text>
 
-            <Title tag='h2'>
-                Ola mundo !!!!!!!!!
-            </Title>
+                        <TextField
+                            fullWidth
+                            textFieldColors={{
+                                neutral: {
+                                    textColor: appConfig.theme.colors.neutrals[200],
+                                    mainColor: appConfig.theme.colors.neutrals[900],
+                                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                                },
+                            }}
+                        />
+                        <Button
+                            type='submit'
+                            label='Entrar'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[500],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                            }}
+                        />
+                    </Box>
+                    {/* Formulário */}
 
-        </div>
-    )
-  }
-  
-  export default HomePage
+
+                    {/* Photo Area */}
+                    <Box
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            maxWidth: '200px',
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            flex: 1,
+                            minHeight: '240px',
+                        }}
+                    >
+                        <Image
+                            styleSheet={{
+                                borderRadius: '50%',
+                                marginBottom: '16px',
+                            }}
+                            src={`https://github.com/${username}.png`}
+                        />
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: appConfig.theme.colors.neutrals[200],
+                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                padding: '3px 10px',
+                                borderRadius: '1000px'
+                            }}
+                        >
+                            {username}
+                        </Text>
+                    </Box>
+                    {/* Photo Area */}
+                </Box>
+            </Box>
+        </>
+    );
+}
